5d243365ccd991f1699938a1d67340f43f096b37
Antoine T <antoine.t5656@gmail.com>
2024-02-23 02:53:36 +0100

    Initial commit

---
+++ src/main/java/uboeventflow/simembreapi/SiMembreApiApplication.java
@@ -0,0 +1,13 @@
+package uboeventflow.simembreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiMembreApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SiMembreApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/uboeventflow/simembreapi/SiMembreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package uboeventflow.simembreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiMembreApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
